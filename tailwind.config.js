eaad46af820a4dcdff89829b9e1572c4694b77fe
0tuedon <tuedonmofe@gmail.com>
2022-01-04 10:23:37 +0100

    Made the folders show on github

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx,html}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,html}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
@@ -7,5 +7,5 @@ module.exports = {
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: ["postcss"]
 };

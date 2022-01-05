b3f4b5e8c0543551201f6998f6135764232488f0
sivle19 <sivle99@protonmail.com>
2022-01-05 20:08:44 +0200

    Created reusable component UploadFile which is used to build test, assignment and attendance upload pages. I used Material icons for icons

--- src/containers/Routing.js
+++ src/containers/Routing.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from '../pages/Home'
 
+
+
 const Routing = () => {
     return (
         <div>
-           <Home/>
+            <Home />
         </div>
     )
 }
---
+++ src/pages/uploadAssignment1.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeaderNav from '../components/HeaderNav'
+import MainNav from '../components/MainNav'
+import UploadFile from '../components/UploadFile'
+
+const UploadAssignment1 = () => (
+
+    <div>
+        <HeaderNav />
+        <MainNav />
+        <UploadFile title="Upload Assignment" text="Import File From Your Computer" classText="fromComputerBtn" icon="arrow_drop_down" />
+    </div >
+
+
+)
+
+
+export default UploadAssignment1
